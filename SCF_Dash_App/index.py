1c1994999ac24b387b1bc1d777b0c0e69e0ddd05
HamzaEric <abubakarhamzamaina@gmail.com>
2026-07-10 10:36:21 +0300

    Change in index.py

--- SCF_Dash_App/index.py
+++ SCF_Dash_App/index.py
@@ -3,6 +3,10 @@ from app import app
 from src.layouts import build_layout
 import src.callbacks  # Crucial: forces callback registration with the app instance
 
+
+server = app.server
+# ------------------------
+
 app.layout = build_layout()
 
 if __name__ == "__main__":
